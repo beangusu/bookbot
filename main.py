144adf834b0384e3807d5620502334575453ca14
beangusu <beangusu@gmail.com>
2026-05-20 19:24:16 -0500

    implemented report printing and sorting char count dictionary

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import word_count
+from stats import (
+    word_count,
+    char_count,
+    sort_chars,
+)
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,6 +12,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
-    print(f"Found {num_words} total words")
+    dictionary = char_count(text)
+    sorted_chars = sort_chars(dictionary)
+    print_report(book_path, num_words, sorted_chars)
+
+def print_report(book_path, word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    text = text.lower()
+    char_dict = {}
+    for char in text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def sort_chars(char_dict):
+    sorted_list = []
+    for char, count in char_dict.items():
+        sorted_list.append({"char": char, "num": count})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+# helper function for sorting the list of characters by their count
+def sort_on(char):
+    return char["num"]
